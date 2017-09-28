8cee92acb0bcdc10b99339b91554fad9d77791da
Aquariuslt <superaquariuslt@gmail.com>
2017-09-29 01:58:49 +0800

    [Refactor] Add some types definitions for jawr locale generator resolver refactor.

--- index.d.ts
+++ index.d.ts
@@ -9,5 +9,38 @@ declare interface JawrOptions {
 
 declare interface JawrContext {
   jawrOptions: JawrOptions;
+  jawrBundles: JawrBundles;
+  jawrBundleMappings: JawrBundleMappings;
 
+
+  getRegisteredJsBundleKeys(): Array;
+
+  getLocaleJsBundles(): Array;
 }
+
+/**
+ * for raw jawr.properties parsed object
+ * */
+declare interface JawrBundles {
+  id?: string;
+
+  composite?: boolean;
+  child?: {
+    names: string
+  };
+
+  global?: boolean;
+  debugnever?: boolean
+  debugonly?: boolean;
+
+  mappings?: string;
+  dependencies?: string;
+}
+
+declare interface JawrBundleMappings {
+  [jawrBundleKey: string]: {
+    id?: string;
+    mappings: Array<string>,
+  }
+}
+
